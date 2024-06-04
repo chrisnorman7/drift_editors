4c40374f077a91a73a9e435e436aa579d4bd2b55
Chris Norman <chris.norman1989@outlook.com>
2024-06-04 10:10:39 +0100

    Added `DoubleColumnHandler`.

---
+++ lib/src/column_handlers/double_column_handler.dart
@@ -0,0 +1,54 @@
+import 'package:backstreets_widgets/widgets.dart';
+import 'package:flutter/material.dart';
+
+import '../types.dart';
+import 'column_handler.dart';
+
+/// A column handler for a [double] [value].
+class DoubleColumnHandler extends ColumnHandler<double> {
+  /// Create an instance.
+  DoubleColumnHandler({
+    required super.column,
+    required super.value,
+    super.title,
+    this.subtitle,
+    this.decimalPlaces = 2,
+    this.min,
+    this.max,
+    this.modifier = 1.0,
+  });
+
+  /// The subtitle for the [DoubleListTile].
+  final String? subtitle;
+
+  /// The number of decimal places to use.
+  final int decimalPlaces;
+
+  /// The minimum value.
+  final double? min;
+
+  /// The maximum value.
+  final double? max;
+
+  /// The modifier to use.
+  final double modifier;
+
+  /// Get the widget.
+  @override
+  Widget getWidget({
+    required final BuildContext context,
+    required final bool autofocus,
+    required final OnChanged<double?> onChanged,
+  }) =>
+      DoubleListTile(
+        value: value ?? 0.0,
+        onChanged: onChanged,
+        title: getTitle(),
+        subtitle: subtitle,
+        autofocus: autofocus,
+        decimalPlaces: decimalPlaces,
+        min: min,
+        max: max,
+        modifier: modifier,
+      );
+}
